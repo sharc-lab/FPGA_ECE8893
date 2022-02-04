55844c255ad66d9034917e928227789aca258034
lakshmi-sathi <58559090+lakshmi-sathi@users.noreply.github.com>
2022-02-04 12:36:20 +0530

    Update top.cpp

--- Lab1/top.cpp
+++ Lab1/top.cpp
@@ -2,9 +2,10 @@
 
 void matrix_mul( FIX_TYPE MatA_DRAM[M][N], FIX_TYPE MatB_DRAM[N][K], FIX_TYPE MatC_DRAM[M][K])
 {
-#pragma HLS interface m_axi port=MatA_DRAM offset=slave bundle=mem
-#pragma HLS interface m_axi port=MatB_DRAM offset=slave bundle=mem
-#pragma HLS interface m_axi port=MatC_DRAM offset=slave bundle=mem
+#pragma HLS interface m_axi depth=100000 port=MatA_DRAM offset=slave bundle=mem
+#pragma HLS interface m_axi depth=100000 port=MatB_DRAM offset=slave bundle=mem
+#pragma HLS interface m_axi depth=100000 port=MatC_DRAM offset=slave bundle=mem
+#pragma HLS interface s_axilite port=return
     
     FIX_TYPE MatA[M][N];
     FIX_TYPE MatB[N][K];
