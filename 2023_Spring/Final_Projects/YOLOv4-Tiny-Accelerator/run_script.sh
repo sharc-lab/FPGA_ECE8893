8171dc9cc1490d6b0419d3c36040f14c19ad4706
Akshay Kamath <89402265+akshay-k2@users.noreply.github.com>
2023-05-12 01:00:43 -0400

    Add files via upload

---
+++ 2023_Spring/Final_Projects/YOLOv4-Tiny-Accelerator/run_script.sh
@@ -0,0 +1,8 @@
+source /tools/software/xilinx/setup_env.sh
+
+source /tools/software/xilinx/Vitis_HLS/latest/settings64.sh
+
+alias vitis_hls="/tools/software/xilinx/Vitis_HLS/latest/bin/vitis_hls"
+alias vivado="/tools/software/xilinx/Vivado/latest/bin/vivado"
+
+
