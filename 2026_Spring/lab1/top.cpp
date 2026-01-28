11b5f5e8a4ec5ff6eaee156d2c723e4ea78d06a3
Hao <callie.hao@gatech.edu>
2026-01-28 10:08:59 -0500

    adding data movement

--- 2026_Spring/lab1/top.cpp
+++ 2026_Spring/lab1/top.cpp
@@ -2,10 +2,24 @@
 
 // Baseline implementation for HLS.
 // Students will optimize this (loops, memory access, etc.).
-void top_kernel(data_t A[N_ROWS][N_COLS],
-                data_t C[N_ROWS][N_COLS]) {
+void top_kernel(data_t A_DRAM[N_ROWS][N_COLS],
+                data_t C_DRAM[N_ROWS][N_COLS]) {
+#pragma HLS interface m_axi port=A_DRAM offset=slave bundle=A
+#pragma HLS interface m_axi port=C_DRAM offset=slave bundle=C
+#pragma HLS interface s_axilite port=return
+
+    // On-chip buffers for A_DRAM and C_DRAM
+    data_t A[N_ROWS][N_COLS];
+    data_t C[N_ROWS][N_COLS];
+
+    for (int i = 0; i < N_ROWS; i++) {
+        for (int j = 0; j < N_COLS; j++) {
+            A[i][j] = A_DRAM[i][j];
+        }
+    }
+
     // Intermediate buffer for row-normalized values
-    static data_t tmp[N_ROWS][N_COLS];
+    data_t tmp[N_ROWS][N_COLS];
 
     // Phase 1: Row-wise normalization
     for (int i = 0; i < N_ROWS; i++) {
@@ -40,6 +54,12 @@ void top_kernel(data_t A[N_ROWS][N_COLS],
         // Apply scale to each element in the column
         for (int i = 0; i < N_ROWS; i++) {
             C[i][j] = tmp[i][j] * scale;
+        }
+    }
+    
+    for (int i = 0; i < N_ROWS; i++) {
+        for (int j = 0; j < N_COLS; j++) {
+            C_DRAM[i][j] = C[i][j];
         }
     }
 }
